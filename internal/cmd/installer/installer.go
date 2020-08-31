cf8f14b406d6221eed75348cf45cc9d0223b96f7
veith <veithz@gmail.com>
2020-08-31 22:23:14 +0200

    installer init

--- internal/cmd/installer/installer.go
+++ internal/cmd/installer/installer.go
@@ -2,8 +2,11 @@ package installer
 
 import (
 	"fmt"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"os"
 	"strings"
 )
 
@@ -13,14 +16,27 @@ func Run(cmd *cobra.Command, args []string) {
 	fmt.Println(deps)
 	for _, d := range deps {
 		strings.Split(d, " ")
+
 		// check it ends with .git
 
 		// create path if it does not exist
 
 		// clone if it is new
 
-		// git pull
+		_, err := git.PlainClone("installedSpecs/nn", false, &git.CloneOptions{
+			URL:      "git://github.com/theNorstroem/furoBaseSpecs.git",
+			Progress: os.Stdout,
+		})
+		// We instantiate a new repository targeting the given path (the .git folder)
+		r, err := git.PlainOpen("installedSpecs/nn")
+		r.Fetch(&git.FetchOptions{})
 
+		// Get the working directory for the repository
+		w, err := r.Worktree()
+		err = w.Checkout(&git.CheckoutOptions{Hash: plumbing.NewHash("v1.10.0")})
+
+		// git pull
+		fmt.Println(err)
 		// checkout version
 	}
 }
