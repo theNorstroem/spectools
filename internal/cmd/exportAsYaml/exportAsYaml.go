3620fabfc5811790faa7c0a9ee410ec3869198a2
veith <veithz@gmail.com>
2020-09-02 10:40:00 +0200

    export full tree as yaml

--- internal/cmd/exportAsYaml/exportAsYaml.go
+++ internal/cmd/exportAsYaml/exportAsYaml.go
@@ -6,34 +6,34 @@ import (
 	"github.com/spf13/viper"
 	"github.com/theNorstroem/spectools/pkg/ast/serviceAst"
 	"github.com/theNorstroem/spectools/pkg/ast/typeAst"
-	"github.com/theNorstroem/spectools/pkg/clientspec"
+	"github.com/theNorstroem/spectools/pkg/specSpec"
 	"github.com/theNorstroem/spectools/pkg/util"
 	"gopkg.in/yaml.v3"
 )
 
 func Run(cmd *cobra.Command, args []string) {
 
-	allTypes := map[string]*clientspec.Type{}
+	allTypes := map[string]*specSpec.Type{}
 	Typelist := &typeAst.Typelist{}
 	Typelist.LoadTypeSpecsFromDir(viper.GetString("typeSpecDir"))
 	Typelist.LoadInstalledTypeSpecsFromDir(util.GetDependencyList()...)
 
 	for k, t := range Typelist.TypesByName {
-		allTypes[k] = clientspec.CreateFromAstType(&t.TypeSpec)
+		allTypes[k] = &t.TypeSpec
 	}
 	for k, t := range Typelist.InstalledTypesByName {
-		allTypes[k] = clientspec.CreateFromAstType(&t.TypeSpec)
+		allTypes[k] = &t.TypeSpec
 	}
 
-	allServices := map[string]*clientspec.Service{}
+	allServices := map[string]*specSpec.Service{}
 	Servicelist := &serviceAst.Servicelist{}
 	Servicelist.LoadInstalledServiceSpecsFromDir(util.GetDependencyList()...)
 	Servicelist.LoadServiceSpecsFromDir(viper.GetString("serviceSpecDir"))
 	for k, t := range Servicelist.ServicesByName {
-		allServices[k] = clientspec.CreateSectviceFromAstService(&t.ServiceSpec)
+		allServices[k] = &t.ServiceSpec
 	}
 	for k, t := range Servicelist.InstalledServicesByName {
-		allServices[k] = clientspec.CreateSectviceFromAstService(&t.ServiceSpec)
+		allServices[k] = &t.ServiceSpec
 	}
 
 	output := map[string]interface{}{}
