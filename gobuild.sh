de25f7b5e35a9cfd430595fc286378b553b8db0e
veith <veithz@gmail.com>
2020-09-03 21:36:34 +0200

    test feat gobuild init

--- gobuild.sh
+++ gobuild.sh
@@ -2,15 +2,4 @@
 
 set -eux
 
-export GOPATH="$(pwd)/.gobuild"
-SRCDIR="${GOPATH}/src/github.com/theNorstroem/spectools"
-
-[ -d ${GOPATH} ] && rm -rf ${GOPATH}
-mkdir -p ${GOPATH}/{src,pkg,bin}
-mkdir -p ${SRCDIR}
-cp main.go ${SRCDIR}
-(
-    echo ${GOPATH}
-    cd ${SRCDIR}
-    go install .
-)
+go install .
