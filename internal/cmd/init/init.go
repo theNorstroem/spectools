e4de2c7eeb9a187056d83803d1d2e2c4f4702569
veith <veithz@gmail.com>
2020-09-07 07:34:10 +0200

    feat: init

---
+++ internal/cmd/init/init.go
@@ -0,0 +1,13 @@
+package init
+
+import "github.com/spf13/cobra"
+
+func Run(cmd *cobra.Command, args []string) {
+	// check if .spectools file exist
+
+	// create .spectools with defaults
+
+	// copy "sample" scripts
+
+	//
+}
