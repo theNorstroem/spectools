b824ab7adf9c939e1e6a5b5a150918abd0ea99b6
veith <veithz@gmail.com>
2020-11-15 17:34:12 +0100

    autocommit publish docs

---
+++ scripts/publish.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# remove old search indexes
+rm docs/en.search*
+# build docs
+hugo -d ./docs
+# add changes
+git add docs/.
+# autocommit
+git commit -m "autocommit publish docs"
+# publish by pushing changes
+git push
