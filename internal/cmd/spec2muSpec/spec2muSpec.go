4c12769cb094db690053474545667c0a3e9c5ece
veith <veithz@gmail.com>
2021-03-03 22:41:20 +0100

    fix: regex fixed

--- internal/cmd/spec2muSpec/spec2muSpec.go
+++ internal/cmd/spec2muSpec/spec2muSpec.go
@@ -86,7 +86,7 @@ func Run(cmd *cobra.Command, args []string) {
 
 	// build the new name and ast map
 	for _, t := range microTypesList.MicroTypes {
-		regex := regexp.MustCompile(`^([^#(]*):?([^#]*)?(#(.*))?$`)
+		regex := regexp.MustCompile(`(?s)^([^#(]*):?([^#]*)?(#(.*))?$`)
 		matches := regex.FindStringSubmatch(t.Type)
 		if len(matches) == 0 {
 			fmt.Println("typeline not parseable", t.Type)
